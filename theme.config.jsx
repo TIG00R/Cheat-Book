a590e771e214e1c714135f6d58e12f0ffcdaad86
TIG00R <60291626+TIG00R@users.noreply.github.com>
2023-03-30 05:19:29 +0200

    test title

--- theme.config.jsx
+++ theme.config.jsx
@@ -3,15 +3,9 @@ export default {
     logo: <strong>Cheat Book</strong>,
     project: {
       link: 'https://github.com/TIG00R/Cheat-Book',
-    },head: (
-      <>
+    },head: <>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta property="og:title" content="Hackers Cheat Book" />
         <meta property="og:description" content="A cheat book for fellow hackers to not waste their precious time <3" />
       </>
-    ), useNextSeoProps() {
-      return {
-        titleTemplate: 'Cheat Book'
-      }
-    }
   }
